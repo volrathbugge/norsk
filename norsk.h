925d0739495d7521de2ec8d6c9ca57961930ad10
volrathbugge <33664285+volrathbugge@users.noreply.github.com>
2022-09-28 16:29:12 +0200

    first outcast

---
+++ norsk.h
@@ -0,0 +1,38 @@
+#ifndef NORSK_H
+#define NORSK_H
+
+#define konst const
+#define strukt struct
+#define størrelsenav sizeof
+
+#define gjør do
+#define mens while
+#define hvis if
+#define ellers else
+#define eller ||
+#define og &&
+#define ikke !
+#define gåtil goto
+#define bryt break
+#define fortsett continue
+#define bytte switch
+#define tilfelle case
+#define retuner return
+
+#define standard default
+#define statisk static
+#define utenfor extern
+#define tom void
+#define flyktig volatile
+
+#define flyt float
+#define dobbel double
+#define kort short
+#define karakt char
+#define heltall int
+#define lang long
+#define fortegn signed 
+#define fortegnsløs unsigned
+#define opptell enum 
+
+#endif NORSK_H
