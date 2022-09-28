ada89b4940745ac4a4fb18bd0b12694d3a0b71af
Jonas Sundseth <jonas.sundseth@gmail.com>
2022-09-28 17:41:57 +0200

    Added support for Nynorsk to C/C++

---
+++ nynorsk.h
@@ -0,0 +1,39 @@
+#ifndef NORSK_H
+#define NORSK_H
+
+#define konst const
+#define strukt struct
+#define storleikentil sizeof
+
+#define gjer do
+#define medan while
+#define viss if
+#define ellers else
+#define eller ||
+#define og &&
+#define ikkje !
+#define faretil goto
+#define bryt break
+#define heldfram continue
+#define byte switch
+#define tilfelle case
+#define returner return
+
+#define standard default
+#define statisk static
+#define utanfor extern
+#define tom void
+#define flyktig volatile
+
+#define flyt float
+#define dobbel double
+#define kort short
+#define karakt char
+#define heiltal int
+#define lang long
+#define kjempelang long long
+#define forteikn signed 
+#define forteiknslaus unsigned
+#define opptell enum 
+
+#endif  // NORSK_H
