0280b19617a4f5279ff3fafde33548862aef4e62
Jonas Sundseth <jonas.sundseth@gmail.com>
2022-09-28 19:47:00 +0200

    Added support for roeverspraak

--- README.md
+++ README.md
@@ -41,3 +41,23 @@ heiltal main()
     returner 0;
 }
 ```
+
+```C
+#include <roeverspraak.h>
+#include <stdio.h>
+
+hoheloltotalollol main()
+{
+    hoheloltotalollol n = 100;
+    fofororetota{
+        hohvovisos ((n % 2) == 0) {
+            printf("%d -> partall\n", n);
+        } elollolerorsos {
+            printf("%d -> oddetall\n", n);
+        }
+        n--;
+    }
+    momenonsos(n > 0);
+    roretoturornoneror 0;
+}
+```
---
+++ roeverspraak.h
@@ -0,0 +1,38 @@
+#ifndef ROEVERSPRAAK_H
+#define ROEVERSPRAAK_H
+
+#define kokononsostot const
+#define sostotrorukoktot struct
+#define bobroredoddodenonavov sizeof
+
+#define fofororetota do
+#define momenonsos while
+#define hohvovisos if
+#define elollolerorsos else
+#define elolloleror ||
+#define ogogog &&
+#define ikokkoke !
+#define sospopasoserortotilol goto
+#define bobrorytot break
+#define foforortotsosetottot continue
+#define bobytottote switch
+#define totilolfofelollole case
+#define roretoturornoneror return
+
+#define sostotanondodarordod default
+#define sostotatotisoskok static
+#define utotenonfoforor extern
+#define totomom void
+#define foflolykoktotigog volatile
+
+#define foflolytot float
+#define dodobobbobelol double
+#define kokorortotkort short
+#define kokarorakoktot char
+#define hoheloltotalollol int
+#define lolanongog long
+#define foforortotegognon signed 
+#define foforortotegognonsosfofrori unsigned
+#define opoppoptotelollol enum 
+
+#endif  // ROEVERSPRAAK_H
